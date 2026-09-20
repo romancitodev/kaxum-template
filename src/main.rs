da7bab2da1fbac034094508e1f1aa527229e3ce9
romandev <84428770+romancitodev@users.noreply.github.com>
2026-09-20 18:23:29 -0300

    added security stuff and rolling updates to the base

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod routes;
 
 use axum::Router;
 use futures::FutureExt;
+use tokio::signal;
 
 use std::future::{Future, IntoFuture};
 use std::net::SocketAddr;
@@ -45,6 +46,34 @@ fn setup(app: Router) -> impl Future<Output = std::io::Result<()>> {
     .map(|bind| bind.expect("Failed to bind to address"))
     .then(move |listener| {
       tracing::info!("ℹ️ | Server listening on {address}");
-      axum::serve(listener, app).into_future()
+      axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown())
+        .into_future()
     })
+}
+
+async fn shutdown() {
+  let ctrl_c = async {
+    signal::ctrl_c()
+      .await
+      .expect("Failed to install Ctrl+C handler");
+  };
+
+  #[cfg(unix)]
+  let terminate = async {
+    signal::unix::signal(signal::unix::SignalKind::terminate())
+      .expect("Failed to install SIGTERM handler")
+      .recv()
+      .await;
+  };
+
+  #[cfg(not(unix))]
+  let terminate = std::future::pending::<()>();
+
+  tokio::select! {
+      _ = ctrl_c => {},
+      _ = terminate => {},
+  }
+
+  tracing::info!("shutdown signal received, draining connections");
 }
